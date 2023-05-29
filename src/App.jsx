4e2f87fe06fb33282bc2226a5316eec6fbc5093e
Sebastian009w <ia3266595@gmail.com>
2023-05-28 20:51:38 -0500

    array Object dnd #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function App() {
+  const [person, setPerson] = useState([
+    {
+      id: 1,
+      name: "Jhon",
+    },
+    {
+      id: 2,
+      name: "Sebastian",
+    },
+    {
+      id: 3,
+      name: "Juan",
+    },
+  ]);
   return <h1>App</h1>;
 }
 
