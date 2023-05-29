ec24f02dcd08e196f0f6363580425b5b37aebedb
Sebastian009w <ia3266595@gmail.com>
2023-05-28 22:00:17 -0500

    drogable and utilities #9

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,18 +1,21 @@
 import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { Card, CardContent, Typography } from "@mui/material";
 
 export function Person({ person }) {
-  const { attributes, listeners, setNodeRef, transform } = useSortable({
-    id: person.id,
-  });
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id: person.id,
+    });
+
+  const css = {
+    marginTop: "1rem",
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   return (
-    <article
-      style={{ marginTop: "1rem" }}
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-    >
+    <article style={css} ref={setNodeRef} {...attributes} {...listeners}>
       <Card sx={{ minWidth: 275, height: 100 }}>
         <CardContent>
           <Typography sx={{ fontSize: 14, color: "text.secondary" }}>
